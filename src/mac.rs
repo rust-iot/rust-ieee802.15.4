7ed76661038e3562f66eada335008c78cf29b3fa
Hanno Braun <hanno@braun-robotics.com>
2018-09-25 12:32:49 +0200

    Add method to write `Address` into a buffer

--- src/mac.rs
+++ src/mac.rs
@@ -1,6 +1,12 @@
 //! Implementation of the IEEE 802.15.4 MAC layer
 
 
+use byteorder::{
+    ByteOrder,
+    LittleEndian,
+};
+
+
 /// PAN ID and short address
 pub struct Address {
     /// The PAN ID
@@ -8,4 +14,16 @@ pub struct Address {
 
     /// 16-bit short address
     pub short_addr: u16,
+}
+
+impl Address {
+    /// Writes the address into a buffer
+    ///
+    /// # Panics
+    ///
+    /// Panics, if the buffer is less than 4 bytes long.
+    pub fn write(&self, buf: &mut [u8]) {
+        LittleEndian::write_u16(buf, self.pan_id);
+        LittleEndian::write_u16(&mut buf[2..], self.short_addr);
+    }
 }
